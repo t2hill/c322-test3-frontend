fc898acd7f9b38989fe9ce7d7926cfd1a51290b0
toohill.ted@gmail.com <toohill.ted@gmail.com>
2024-04-20 20:01:11 -0400

    test 3 solutions

--- configuration.js
+++ configuration.js
@@ -54,16 +54,16 @@ async function signup() {
     let email = document.getElementById("email").value;
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    let customer = {email:email, username: username, password: password}
+    let customer = {email:email, username: username, password: password};
     let request = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(customer)
       };
       try {
-        let response = await fetch(getHost() + "/signup", request);
+        let response = await fetch("http://localhost:8080/signup", request);
         if(response.status == 200) {  
             alert("The registration was successful!")
             location.href = "login.html";
@@ -79,21 +79,20 @@ async function signup() {
       }    
 }
 
-
-
 async function login() {    
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let customer = {username: username, password: password}
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+    let customer = {username: username, password: password};
+    console.log(configuration.token());
     let request = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(customer)
       };
       try {
-        let response = await fetch(getHost() + "/signin", request);
+        let response = await fetch("http://localhost:8080/signin", request);
         if(response.status == 200) {  
             alert("The login was successful!");
             const token = await response.text();
